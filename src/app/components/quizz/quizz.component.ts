e0fd1592ed15ad939140d270c44abc3334faa2c6
Wellysson Rocha <33838737+Werisu@users.noreply.github.com>
2023-03-30 17:13:46 -0300

    :rocket: FEAT: bootstrap, button reset

--- src/app/components/quizz/quizz.component.ts
+++ src/app/components/quizz/quizz.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Question, Quizz } from 'src/app/pages/home/interfaces/quizz';
-import quizz_questions from "../../../assets/data/quizz_questions.json";
+import quizz_questions from '../../../assets/data/quizz_questions.json';
 
 @Component({
   selector: 'app-quizz',
   templateUrl: './quizz.component.html',
-  styleUrls: ['./quizz.component.css']
+  styleUrls: ['./quizz.component.css'],
 })
 export class QuizzComponent implements OnInit {
   public title: string = '';
@@ -19,12 +19,12 @@ export class QuizzComponent implements OnInit {
   public questionIndex: number = 0;
   public questionMaxIndex: number = 0;
 
-  public finished:boolean = false;
+  public finished: boolean = false;
 
   constructor() {}
 
   public ngOnInit(): void {
-    if(quizz_questions){
+    if (quizz_questions) {
       this.finished = false;
       this.title = quizz_questions.title;
 
@@ -36,26 +36,32 @@ export class QuizzComponent implements OnInit {
     }
   }
 
-  public playerChoose(value: string){
+  public playerChoose(value: string) {
     this.answers.push(value);
-    this.nexStep()
+    this.nexStep();
   }
 
-  public async nexStep(){
-    this.questionIndex +=1;
+  public async nexStep() {
+    this.questionIndex += 1;
 
     if (this.questionMaxIndex > this.questionIndex) {
-      this.questionSelected = this.questions[this.questionIndex]
+      this.questionSelected = this.questions[this.questionIndex];
     } else {
       const finalAnswer: string = await this.checkResult(this.answers);
       this.finished = true;
-      this.answerSelected = quizz_questions.results[finalAnswer as keyof typeof quizz_questions.results]
+      this.answerSelected =
+        quizz_questions.results[
+          finalAnswer as keyof typeof quizz_questions.results
+        ];
     }
   }
 
-  async checkResult(anwsers: string[]){
+  async checkResult(anwsers: string[]) {
     const result = anwsers.reduce((previous, current, i, arr) => {
-      if (arr.filter(item => item === previous).length > arr.filter(item => item === current).length) {
+      if (
+        arr.filter((item) => item === previous).length >
+        arr.filter((item) => item === current).length
+      ) {
         return previous;
       } else {
         return current;
@@ -65,4 +71,13 @@ export class QuizzComponent implements OnInit {
     return result;
   }
 
+  public resetQuizz() {
+    this.finished = false;
+    this.questionIndex = 0;
+
+    this.questionSelected = this.questions[this.questionIndex];
+    this.title = quizz_questions.title;
+    this.answers = [];
+    this.answerSelected = '';
+  }
 }
